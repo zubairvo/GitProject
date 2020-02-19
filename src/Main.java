4d2262f69815771eb90a6923403e0b7b7dcd36d1
zubairvo <49389915+zubairvo@users.noreply.github.com>
2020-02-19 13:33:57 +0200

    update Main Class
    Edited 3rd SOUT

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("I am your first sentence");
         System.out.println("Hello World!");
-        System.out.println("Changes in my life!");
+        System.out.println("Changes in my life! I won't leave you behind");
     }
 }
